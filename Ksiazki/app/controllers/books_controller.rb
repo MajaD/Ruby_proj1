07545623a6088890a997490cf7f16ce6c5525982
MajaD <Dark-Angel93@wp.pl>
2016-03-27 20:27:45 +0200

    dodanie wyswietlania ksiazki

--- Ksiazki/app/controllers/books_controller.rb
+++ Ksiazki/app/controllers/books_controller.rb
@@ -1,7 +1,10 @@
 class BooksController < ApplicationController
-
+before_action :find_book, only: [:show, :edit, :update, :destroy]
 	def index
 		@books = Book.all.order("created_at DESC")
+	end
+
+	def show
 	end
 
 	def new
@@ -22,6 +25,10 @@ class BooksController < ApplicationController
 
 		def book_params
 			params.require(:book).permit(:title, :author, :description)
+		end
+
+		def find_book
+			@book = Book.find(params[:id])
 		end
 
 end
